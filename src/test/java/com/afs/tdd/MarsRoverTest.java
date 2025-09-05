cd168ba5099995405a56ebf31ac0cc5fd1b12e0d
SINJE2 <SINJE2@OOCL.COM>
2025-09-05 16:28:57 +0800

    test: moving East direction test case

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
     @Test
-    void should_move_when_executeCommand_given_M(){
+    void should_move_when_executeCommand_given_M_and_Direction_is_N(){
         // Given
         Location location = new Location(0,0, Direction.N);
         MarsRover marsRover = new MarsRover(location);
@@ -14,6 +14,20 @@ class MarsRoverTest {
         marsRover.executeCommand(Command.M);
         //Then
         Location currentLocation = new Location(0, 1, Direction.N);
+        assertEquals(currentLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(currentLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+
+    @Test
+    void should_move_when_executeCommand_given_M_and_Direction_is_E(){
+        // Given
+        Location location = new Location(0,0, Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.M);
+        //Then
+        Location currentLocation = new Location(1, 0, Direction.E);
         assertEquals(currentLocation.getLocationY(), marsRover.getLocation().getLocationY());
         assertEquals(currentLocation.getLocationX(), marsRover.getLocation().getLocationX());
         assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
