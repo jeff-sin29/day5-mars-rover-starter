4ace2f466fe603462626a17f1f1757c777f13745
SINJE2 <SINJE2@OOCL.COM>
2025-09-07 01:23:40 +0800

    test: add testcase for moveBackward in East direction

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -186,4 +186,18 @@ class MarsRoverTest {
         assertEquals(currentLocation.getLocationX(), marsRover.getLocation().getLocationX());
         assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
     }
+
+    @Test
+    void should_mov_backward_when_executeCommand_given_B_and_Direction_is_E(){
+        // Given
+        Location location = new Location(0,0, Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.B);
+        //Then
+        Location currentLocation = new Location(-1, 0, Direction.E);
+        assertEquals(currentLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(currentLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
 }
